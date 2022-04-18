bc74290f6c741bd8e0c8d6e401ffc5ea568ecf02
Hermit-77 <75745771+hermit-77@users.noreply.github.com>
2022-04-18 19:00:39 +0100

    Update server.php

--- api/server.php
+++ api/server.php
@@ -19,3 +19,6 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 }
 
 require_once __DIR__.'/public/index.php';
+
+
+//require_once __DIR__.'/public/expert/MLP/ to use the structural database to save and crawl data for users to have perfect matching algorithm.//
